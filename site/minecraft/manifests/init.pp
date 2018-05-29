cc886328f1eb1f98ca9cd7782a35076b4dd6219c
tylereh <39161224+tylereh@users.noreply.github.com>
2018-05-29 11:29:05 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,4 +17,8 @@ class minecraft {
     ensure => file,
     source => 'puppet:///module/minecraft/minecraft.service',
   }  
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }  
  }
