e9495a7ef063a4faa92af86640f485f41120bcfe
tylereh <39161224+tylereh@users.noreply.github.com>
2018-05-29 10:36:35 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDUAMhNYyTqEYuSxCGVsklmrLrD769tFqf+CRXsrF5KCK67xuaAb+0WgHbOHjySfNHNZswY2sWdTBItEMiGRhxILcVuse2x0zrT2934+2QZHKka/Xf4l4oaLw00+gf7tjyBmlkMnd1TRMa6YY37haUqdJUdd1BRKjuUiQyOq7lup79AJoQznt8VyrVqhmyqAs24762XprY0EpJBh640DK+cHfULTKXDQuK1xHAT7RHHYS+Z46vfbFBUj5SLAO2Qw3pbj8wXMdZf4BxIr2KxocA/UJkrMhySCsYgXOIoW/zysu98+wlStsAsZr4zACkH2Ev4ndaS0gAp5PKQihe5I0g7',
 	}  
 }
